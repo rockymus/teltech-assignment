d715b92d3495c4a2799fa820b689b654116a419e
rgrubelic <teslajehrvat>
2023-01-21 16:45:27 +0100

    initial setup of spring project

---
+++ src/test/java/com/teltech/mathapp/MathAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teltech.mathapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
